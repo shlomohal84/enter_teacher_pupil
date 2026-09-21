ae7f2e3edce98bea600cf238ea84175a63bf6ac8
Shlomo Halperin <halperinshlomo@gmail.com>
2026-09-21 20:15:04 +0300

    Interactively show and hide nav menus depnding if user is logged in

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -13,17 +12,34 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import { Link as RouterLink } from "react-router-dom";
+import { useAuth } from "#src/hooks/useAuth.js";
+import { useState } from "react";
 
-const pages = [
-	{ title: "Home", path: "/" },
-	{ title: "Login", path: "/login" },
-	{ title: "Register", path: "/register" },
+const navigationLinks = [
+	{ title: "Home", path: "/", type: "always" },
+	{ title: "Login", path: "/login", type: "guest" },
+	{ title: "Register", path: "/register", type: "guest" },
+	{ title: "Logout", path: "#", type: "auth" },
 ];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
-	const [anchorElNav, setAnchorElNav] = React.useState(null);
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const { user, isLoading, logout } = useAuth();
+	const [anchorElNav, setAnchorElNav] = useState(null);
+	const [anchorElUser, setAnchorElUser] = useState(null);
+
+	const filteredPages = navigationLinks.filter((page) => {
+		if (isLoading) return page.type === "always";
+		if (user) return page.type === "always" || page.type === "auth";
+		return page.type === "always" || page.type === "guest";
+	});
+
+	const handlePageClick = (pageTitle) => {
+		handleCloseNavMenu();
+		if (pageTitle === "Logout" && logout) {
+			logout();
+		}
+	};
 
 	const handleOpenNavMenu = (event) => {
 		setAnchorElNav(event.currentTarget);
@@ -90,11 +106,11 @@ function ResponsiveAppBar() {
 							onClose={handleCloseNavMenu}
 							sx={{ display: { xs: "block", md: "none" } }}
 						>
-							{pages.map((page) => (
+							{filteredPages.map((page) => (
 								<MenuItem
-									component={RouterLink}
+									component={page.title === "Logout" ? "div" : RouterLink}
 									key={page.title}
-									onClick={handleCloseNavMenu}
+									onClick={() => handlePageClick(page.title)}
 									to={page.path}
 								>
 									<Typography sx={{ textAlign: "center" }}>
@@ -124,10 +140,10 @@ function ResponsiveAppBar() {
 						LOGO
 					</Typography>
 					<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-						{pages.map((page) => (
+						{filteredPages.map((page) => (
 							<Button
 								key={page.title}
-								onClick={handleCloseNavMenu}
+								onClick={() => handlePageClick(page.title)}
 								sx={{ my: 2, color: "white", display: "block" }}
 								component={RouterLink}
 								to={page.path}
@@ -137,35 +153,42 @@ function ResponsiveAppBar() {
 						))}
 					</Box>
 					<Box sx={{ flexGrow: 0 }}>
-						<Tooltip title="Open settings">
-							<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-								<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-							</IconButton>
-						</Tooltip>
-						<Menu
-							sx={{ mt: "45px" }}
-							id="menu-appbar"
-							anchorEl={anchorElUser}
-							anchorOrigin={{
-								vertical: "top",
-								horizontal: "right",
-							}}
-							keepMounted
-							transformOrigin={{
-								vertical: "top",
-								horizontal: "right",
-							}}
-							open={Boolean(anchorElUser)}
-							onClose={handleCloseUserMenu}
-						>
-							{settings.map((setting) => (
-								<MenuItem key={setting} onClick={handleCloseUserMenu}>
-									<Typography sx={{ textAlign: "center" }}>
-										{setting}
-									</Typography>
-								</MenuItem>
-							))}
-						</Menu>
+						{user && (
+							<>
+								<Tooltip title="Open settings">
+									<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+										<Avatar
+											alt="Remy Sharp"
+											src="/static/images/avatar/2.jpg"
+										/>
+									</IconButton>
+								</Tooltip>
+								<Menu
+									sx={{ mt: "45px" }}
+									id="menu-appbar"
+									anchorEl={anchorElUser}
+									anchorOrigin={{
+										vertical: "top",
+										horizontal: "right",
+									}}
+									keepMounted
+									transformOrigin={{
+										vertical: "top",
+										horizontal: "right",
+									}}
+									open={Boolean(anchorElUser)}
+									onClose={handleCloseUserMenu}
+								>
+									{settings.map((setting) => (
+										<MenuItem key={setting} onClick={handleCloseUserMenu}>
+											<Typography sx={{ textAlign: "center" }}>
+												{setting}
+											</Typography>
+										</MenuItem>
+									))}
+								</Menu>
+							</>
+						)}
 					</Box>
 				</Toolbar>
 			</Container>
